222e3c9259ace7fa18d8af1011bc448f38eba544
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-09-19 14:31:12 +0700

    javascript: object (hash tables / map / dictionary)

---
+++ javascript/object.js
@@ -0,0 +1,20 @@
+/*
+╭── Object (Hash Tables) ──────────────────────────────────────────────────╮
+│  Pada Javascript sebuah Object adalah kumpulan key-value pairs           │
+│  Tipe data ini juga disebut map, dictionary atau hash-table di           │
+│  bahasa pemrograman lain. Keyword untuk membuat Object:                  │
+│  * var_type var_name = {                                                 │
+│       key1: "value1",                                                    │
+│       key2: "value2",                                                    │
+│       key3: "value3",                                                    │
+│    }                                                                     │
+╰──────────────────────────────────────────────────────────────────────────╯
+*/
+
+const obj = {
+  prop1: "I'm",
+  prop2: "an",
+  prop3: "object",
+};
+
+console.log(obj);
